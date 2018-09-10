4f395d74f6fb2e650638d032d69f900ab33e31b6
zhchengzhi2007@163.com <zhchengzhi2007@163.com>
2018-09-10 14:15:27 +0800

    test git function

---
+++ model/model1/src/prepare.py
@@ -0,0 +1,23 @@
+#-*- coding:UTF-8 -*-
+"""模型数据预处理
+"""
+#@filename: prepare
+#@author: liuxh
+#@date: 2018/9/10 14:14
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+
+
+
+
+
+
+
+
+
+
+
+
