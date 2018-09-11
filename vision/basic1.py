163de2128d60ef8785b67eb2a6bda34ef1b6b522
zhchengzhi2007@163.com <zhchengzhi2007@163.com>
2018-09-11 15:52:33 +0800

    极坐标图

--- vision/basic1.py
+++ vision/basic1.py
@@ -7,12 +7,56 @@
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
+import random
 
-plt.rcParams['font.sas-serig']=['SimHei']    #用来正常显示中文标签
-plt.rcParams['axes.unicode_minus']=False     #用来正常显示负号
-
-
-
-
+# plt.rcParams['font.sas-serig']=['SimHei']    #用来正常显示中文标签
+# plt.rcParams['axes.unicode_minus']=False     #用来正常显示负号
 
 
+# labels = np.array(['a','b','c','d','e','f']) # 标签
+# dataLenth = 6 # 数据长度
+# data = np.array([1,4,3,6,4,8]) # 数据
+#
+#
+# N = 20
+# theta = np.linspace(0.0,2*np.pi,N,endpoint = False)
+# radii = 10*np.random.rand(N)
+# width = np.pi / 4*np.random.rand(N)
+#
+# ax = plt.subplot(111,projection = "polor")
+# bars = ax.bar(theta,radii,width=width,bottom=0.0)
+#
+# for r,bar in zip(radii,bars):
+#     bar.set_facecolor(plt.cm.viridis(r/10.0))
+#     bar.set_alpha(0.5)
+#
+# plt.show
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import NullFormatter
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+
+# Compute pie slices
+N = 20
+theta = np.linspace(0.0, 2 * np.pi, N, endpoint=False)
+radii = 10 * np.random.rand(N)
+width = np.pi / 4 * np.random.rand(N)
+
+ax = plt.subplot(111, projection='polar')
+bars = ax.bar(theta, radii, width=width, bottom=0.0)
+
+# Use custom colors and opacity
+for r, bar in zip(radii, bars):
+    bar.set_facecolor(plt.cm.viridis(r / 10.))
+    bar.set_alpha(0.5)
+
+plt.show()
