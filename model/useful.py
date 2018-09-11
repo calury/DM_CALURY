1a71285399fcd818b5e040f69dea2b8e4ab2b24e
zhchengzhi2007@163.com <zhchengzhi2007@163.com>
2018-09-11 11:12:34 +0800

    useful

--- model/useful.py
+++ model/useful.py
@@ -5,6 +5,9 @@
 #@author: liuxh
 #@date: 2018/9/10 17:17
 
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
-
+plt.rcParams['font.sas-serig']=['SimHei']    #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False     #用来正常显示负号
