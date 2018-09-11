060afbc11b8ec1dbdeb2e68c989f94c3d999aa5b
zhchengzhi2007@163.com <zhchengzhi2007@163.com>
2018-09-11 11:09:42 +0800

    vs

---
+++ vision/basic1.py
@@ -0,0 +1,18 @@
+#-*- coding:UTF-8 -*-
+"""
+"""
+#@filename: basic1
+#@author: liuxh
+#@date: 2018/9/11 11:08
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+plt.rcParams['font.sas-serig']=['SimHei']    #用来正常显示中文标签
+plt.rcParams['axes.unicode_minus']=False     #用来正常显示负号
+
+
+
+
+
+
