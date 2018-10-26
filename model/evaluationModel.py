25872025dd2f879e8a75d061d21029160c5b8d04
zhchengzhi2007@163.com <zhchengzhi2007@163.com>
2018-10-26 11:06:32 +0800

    evaluation model 1

--- model/evaluationModel.py
+++ model/evaluationModel.py
@@ -1,6 +1,59 @@
 #-*- coding:UTF-8 -*-
-"""
+"""对模型进行网络搜索进行调做优，
+对建模结果进行评价
 """
 #@filename: evaluationModel
 #@author: liuxh
 #@date: 2018/10/26 11:04
+
+import numpy as np
+import pandas as pd
+import sklearn
+from sklearn.model_selection import GridSearchCV
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
